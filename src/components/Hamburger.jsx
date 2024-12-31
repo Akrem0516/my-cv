a9e8b00a3e635e36bb90d090188b8e3336062de1
akrem0516 <ouassimakrem@gmail.com>
2024-12-31 11:28:12 +0100

    Hamburger edit

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,17 +1,14 @@
 
-import { useState } from 'react';
 import '../styles/Hamburger.css'
 
 export function Hamburger({ onClick, setGlobalHidden, globalHidden }) {
-    const [isActive, setIsActive] = useState(false);
 
     const hamburgerClicked = () => {
         onClick();
         setGlobalHidden(!globalHidden);
-        setIsActive(!isActive)
     }
     return (
-        <button className={`hamburger ${isActive ? 'active' : ''}`} onClick={hamburgerClicked} aria-label="Toggle menu">
+        <button className={`hamburger ${globalHidden ? 'active' : ''}`} onClick={hamburgerClicked} aria-label="Toggle menu">
             <span></span>
             <span></span>
             <span></span>
