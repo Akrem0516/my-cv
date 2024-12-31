08e77488fb09fcc219988dfa56bec16b40d45137
Mezouri Akrem Ouassim <ouassimakrem@gmail.com>
2024-12-31 16:38:03 +0100

    projects onclick

--- src/components/MotionImgWrapper.jsx
+++ src/components/MotionImgWrapper.jsx
@@ -1,13 +1,14 @@
 import { motion } from "framer-motion";
 
 
-export function MotionImgWrapper({ PicPath, Alt, Class }) {
+export function MotionImgWrapper({ PicPath, Alt, Class, Clicked }) {
     return (
         <motion.img src={PicPath} alt={Alt} className={Class}
             initial={{ opacity: 0, scale: 0.8 }} // Image starts small and transparent
             animate={{ opacity: 1, scale: 1 }}   // Fade in and scale to normal
             exit={{ opacity: 0, scale: 0.8 }}
-            transition={{ duration: 0.6, delay: 0.4 }} // Delay for smoother effect
+            transition={{ duration: 0.6, delay: 0.4 }}
+            onClick={Clicked} // Delay for smoother effect
         />
     )
 }
