aa906c543e8ad434b83bcd23609a2c5187577fda
Mezouri Akrem Ouassim <ouassimakrem@gmail.com>
2025-02-19 12:35:50 +0100

    fixing vercel analytics error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Ensuring Router wraps the app
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { App } from './pages/App';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
