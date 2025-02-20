1428610e8954eb6159d20cbdb339157e06fd4119
Mezouri Akrem Ouassim <ouassimakrem@gmail.com>
2025-02-20 13:41:57 +0100

    add delay

--- src/components/MotionDivWrapper.jsx
+++ src/components/MotionDivWrapper.jsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 export function MotionDivWrapper({ children, Class }) {
     return (
         <motion.div className={Class}
-            initial={{ opacity: 0, y: -70 }} // Content starts below the viewport
+            initial={{ opacity: 0, y: -90 }} // Content starts below the viewport
             animate={{ opacity: 1, y: 0 }}  // Slides into view
-            exit={{ opacity: 0, y: 70 }}
-            transition={{ duration: 0.6 }}
+            exit={{ opacity: 0, y: 90 }}
+            transition={{ duration: 0.6, delay: 0.1 }}
         >
             {children}
         </motion.div>
