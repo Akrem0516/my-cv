9cffab7f3b1926994252edc420f963eea3fce7dd
Mezouri Akrem Ouassim <ouassimakrem@gmail.com>
2025-02-24 12:00:35 +0100

    change in the 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,10 @@
-import MeNo from "../assets/404-no.gif"
+import No from "../assets/404-no.gif"
 
 function Notfound() {
     return (
         <div style={{ display: "flex", flexDirection: "column", gap: "30px" }}>
             <h1 style={{ color: "white", margin: "0 auto" }}> Do not be playful </h1>
-            <img style={{ width: "350px", borderRadius: "20px", margin: "0 auto" }} src={MeNo} alt="no" />
+            <img style={{ width: "350px", borderRadius: "20px", margin: "0 auto" }} src={No} alt="no" />
         </div>
     );
 }
