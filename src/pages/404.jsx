7b4a8fdda0b8ad906b5c1a749ba8c8334a5c9268
Mezouri Akrem Ouassim <ouassimakrem@gmail.com>
2025-02-24 12:00:21 +0100

    change the no gif

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,10 @@
-import MeNo from "../assets/MeNo.gif"
+import MeNo from "../assets/404-no.gif"
 
 function Notfound() {
     return (
-        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "30px" }}>
             <h1 style={{ color: "white", margin: "0 auto" }}> Do not be playful </h1>
-            <img style={{ width: "200px", borderRadius: "20px", margin: "0 auto" }} src={MeNo} alt="no" />
+            <img style={{ width: "350px", borderRadius: "20px", margin: "0 auto" }} src={MeNo} alt="no" />
         </div>
     );
 }
